8ec52b49f54bab43948f0ccd0e60a3d8bd82a806
Joel-100 <joelkugonza@gmail.com>
2022-11-18 13:45:52 +0300

    Dispatch an action to display data on the page

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -13,19 +13,23 @@ function Rockets() {
   console.log(rockets);
   return (
     <div className="rocket-container">
-      <div className="rocket-card">
-        <div className="rocket-profile">
-          <img src="" alt="rocket" />
+      {rockets?.map((item) => (
+        <div className="rocket-card" key={item.id}>
+          <div className="rocket-profile">
+            <img src={item.flickr_images} alt="" />
+          </div>
+          <div className="rocket-description">
+            <h1>{item.rockets_name}</h1>
+            <p>
+              <span className="reserved-badge">Reserved</span>
+              {item.description}
+            </p>
+            <button className="btn" type="button">
+              Reserve Rocket
+            </button>
+          </div>
         </div>
-        <div className="rocket-description">
-          <h1>Flight 1</h1>
-          <p>
-            <span className="reserved-badge">Reserved</span>
-            Description
-          </p>
-          <button className="btn" type="button">Reserve Rocket</button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
