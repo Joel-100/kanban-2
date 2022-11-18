042247e0cd0acc768b1c1f22f32f5707e37e76fe
freddykaberuka <freddykaberuka2009@gmail.com>
2022-11-18 13:55:54 +0200

    add basic structure for mission and style

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import './mission.css';
 
-function Mission() {
-  return (
-    <div>Mission</div>
-  );
-}
+const Mission = () => (
+  <div className="missions">
+    <table>
+      <thead className="tableHeader">
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="missionName">Thaicom</td>
+          <td className="missionDescription">askdjaksdajdkasjdkas njdajlas anjnajsdj ajksdnajsdjas njfdasdjas ajdnajxende</td>
+          <td className="btnRow">
+            <span className="activeMember">Active Member </span>
+          </td>
+          <td className="btnRow">
+            <button type="button" className="activeButton">
+              Leave Mission
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
 
 export default Mission;
