bbab04db42f0018164e37b7d6a9f7b9e5b857ead
Joel-100 <joelkugonza@gmail.com>
2022-11-18 13:25:50 +0300

    Dispatch action to display data ain component

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAllRockets } from '../redux/rockets/rocket';
 
 function Rockets() {
+  const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllRockets());
+  }, []);
+  console.log(rockets);
   return (
     <div>Rockets</div>
   );
