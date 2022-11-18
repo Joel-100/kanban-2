fcdb7af0c535fd4ddcac885ea32cb61eca2cb84d
Joel-100 <joelkugonza@gmail.com>
2022-11-18 13:37:45 +0300

    Add layout to rocket page

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllRockets } from '../redux/rockets/rocket';
+import './style.css';
 
 function Rockets() {
   const rockets = useSelector((state) => state.rockets);
@@ -11,7 +12,21 @@ function Rockets() {
   }, []);
   console.log(rockets);
   return (
-    <div>Rockets</div>
+    <div className="rocket-container">
+      <div className="rocket-card">
+        <div className="rocket-profile">
+          <img src="" alt="rocket" />
+        </div>
+        <div className="rocket-description">
+          <h1>Flight 1</h1>
+          <p>
+            <span className="reserved-badge">Reserved</span>
+            Description
+          </p>
+          <button className="btn" type="button">Reserve Rocket</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
